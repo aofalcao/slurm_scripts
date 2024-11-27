fe709584338fc1b2a39b4d8d33b7ee63cc96752b
Andre Falcao <aofalcao@ciencias.ulisboa.pt>
2024-11-27 11:55:49 +0000

    Delete shell/testnv.sh

--- shell/testnv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=test-nvidia-smi
-#SBATCH --output=/mnt/storage/admindi/home/USERNAME/slurm_outputs/nvidia.smi
-#SBATCH --ntasks=1
-#SBATCH --time=00:01:00
-#SBATCH --partition=compute
-
-nvidia-smi
-
