0cce57f787919def08c5955d9ed3cdaad07fd8ac
Andre Falcao <aofalcao@ciencias.ulisboa.pt>
2024-11-27 11:42:44 +0000

    Create twogpus.sh
    Sample slurm script for using more than one GPU

---
+++ shell/twogpus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=simple-job
+#SBATCH --output=/mnt/storage/admindi/home/rgoncalves/slurm_outputs/output.txt
+#SBATCH --ntasks=1
+#SBATCH --partition=compute
+#SBATCH --gres=gpu:2
+
+# Run the Python script
+python sample-manygpus.py
